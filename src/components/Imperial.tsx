33c19c833e9cfd8d65088511e8452d45b2b7f002
Chamu Mutezva <ckmutezva@gmail.com>
2023-08-03 18:38:22 +0200

    update: refactor imperial to use bmiCalculator function

--- src/components/Imperial.tsx
+++ src/components/Imperial.tsx
@@ -7,6 +7,7 @@ import {
 } from "@mui/material";
 // import InputText from "./InputText";
 import { useEffect, useState } from "react";
+import { bmiCalculator } from "../utils/bmiCalculator";
 
 interface Imperial {
   feet: number;
@@ -56,7 +57,6 @@ function Imperial(props: {
       [name]: value,
     });
     props.setBmiImperial(() => total);
-    // console.log(name, value);
   }
 
   const { feet, inch, pounds, stone, total } = currentBmi;
@@ -64,20 +64,15 @@ function Imperial(props: {
   useEffect(() => {
     const totalHeightInch = Number(feet) * 12 + Number(inch);
     const totalWeightPounds = Number(stone) * 14 + Number(pounds);
-    //  console.log(`stones : ${stone} , pounds: ${pounds}`);
-    //  console.log(
-    //    `total weight pounds: ${stone} * 14 + ${pounds}: ${totalWeightPounds}`
-    //  );
-    //  console.log(`feet: ${feet} , inch: ${inch}`);
-    //  console.log(
-    //    `total height inches: ${feet} * 12 + ${inch}: ${totalHeightInch}`
-    //  );
     setCurrentBmi({
       ...currentBmi,
+      //  total: Number(
+      //    ((totalWeightPounds / totalHeightInch / totalHeightInch) * 703).toFixed(
+      //     2
+      //  )
+      // ),
       total: Number(
-        ((totalWeightPounds / totalHeightInch / totalHeightInch) * 703).toFixed(
-          2
-        )
+        bmiCalculator(totalWeightPounds, totalHeightInch, 703).toFixed(2)
       ),
     });
 
